765c0f0daa7948ba2893ee4934684a34aade2c4f
张星宇 <649395594@qq.com>
2015-12-03 00:16:05 +0800

    完善tabbar框架

--- app/src/main/java/com/neu/coder/mathmodeling/MainActivity.java
+++ app/src/main/java/com/neu/coder/mathmodeling/MainActivity.java
@@ -1,8 +1,10 @@
 package com.neu.coder.mathmodeling;
 
+import android.app.LocalActivityManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
@@ -10,14 +12,19 @@ import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private TabBarView tabBarAnimView;
     private RadioGroup main_radiogroup;
     private TabHost tabHost;
-    private RadioButton tab_icon_weixin, tab_icon_address, tab_icon_friend,tab_icon_setting;
+    private RadioButton tab_icon_weixin, tab_icon_address, tab_icon_friend, tab_icon_setting;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("数学建模");
+        setSupportActionBar(toolbar);
         main_radiogroup = (RadioGroup) findViewById(R.id.main_radiogroup);
 
         tab_icon_weixin = (RadioButton) findViewById(R.id.tab_icon_weixin);
@@ -25,23 +32,24 @@ public class MainActivity extends AppCompatActivity {
         tab_icon_friend = (RadioButton) findViewById(R.id.tab_icon_friend);
         tab_icon_setting = (RadioButton) findViewById(R.id.tab_icon_setting);
 
-        //往TabWidget添加Tab
-//        tabhost = getTabHost();
-        tabHost = (TabHost)findViewById(R.id.main_tabhost);
-        checkListener checkradio = new checkListener();
+        tabHost = (TabHost) this.findViewById(R.id.tabhost);
+        LocalActivityManager mLocalActivityManager = new LocalActivityManager(this, false);
+        mLocalActivityManager.dispatchCreate(savedInstanceState);
+        tabHost.setup(mLocalActivityManager);
+
+        CheckListener checkradio = new CheckListener();
         main_radiogroup.setOnCheckedChangeListener(checkradio);
-        tabHost.addTab(tabHost.newTabSpec("tag1").setContent(new Intent(this,MOOCActivity.class)).setIndicator("tab1"));
-//        tabhost.addTab(tabhost.newTabSpec("tag2").setIndicator("1").setContent(new Intent(this,Activity.class)));
-//        tabhost.addTab(tabhost.newTabSpec("tag3").setIndicator("2").setContent(new Intent(this,Activity.class)));
-//        tabhost.addTab(tabhost.newTabSpec("tag4").setIndicator("3").setContent(new Intent(this,Activity.class)));
+
+        tabHost.addTab(tabHost.newTabSpec("tag1").setIndicator("1").setContent(new Intent(this,MOOCActivity.class)));
+        tabHost.addTab(tabHost.newTabSpec("tag2").setIndicator("1").setContent(new Intent(this,ClubActivity.class)));
+        tabHost.addTab(tabHost.newTabSpec("tag3").setIndicator("2").setContent(new Intent(this,QuizActivity.class)));
+        tabHost.addTab(tabHost.newTabSpec("tag4").setIndicator("3").setContent(new Intent(this,ResourcesActivity.class)));
+
     }
 
-    public class checkListener implements OnCheckedChangeListener{
+    public class CheckListener implements OnCheckedChangeListener {
         @Override
         public void onCheckedChanged(RadioGroup group, int checkedId) {
-            // TODO Auto-generated method stub
-            //setCurrentTab 通过标签索引设置当前显示的内容
-            //setCurrentTabByTag 通过标签名设置当前显示的内容
             switch(checkedId){
                 case R.id.tab_icon_weixin:
                     tabHost.setCurrentTab(0);
@@ -56,8 +64,6 @@ public class MainActivity extends AppCompatActivity {
                     tabHost.setCurrentTab(3);
                     break;
             }
-
-
         }
     }
 }
---
+++ app/src/main/java/com/neu/coder/mathmodeling/Utils/Utils.java
@@ -0,0 +1,10 @@
+package com.neu.coder.mathmodeling.Utils;
+
+/**
+ * Created by zxy on 15/12/2.
+ */
+public class Utils {
+    public static void MyLog(){
+
+    }
+}
