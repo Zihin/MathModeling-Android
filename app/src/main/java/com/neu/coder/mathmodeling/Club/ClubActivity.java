df132654877bb1d8d5d857585cfb7a2d403686bc
ZihinTse <411509067@qq.com>
2016-01-10 19:51:22 +0800

    Add TabStrip

--- app/src/main/java/com/neu/coder/mathmodeling/Club/ClubActivity.java
+++ app/src/main/java/com/neu/coder/mathmodeling/Club/ClubActivity.java
@@ -1,15 +1,42 @@
 package com.neu.coder.mathmodeling.Club;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
+import com.gxz.PagerSlidingTabStrip;
 import com.neu.coder.mathmodeling.R;
 
+import java.util.ArrayList;
+
+/**
+ * Created by Zihin on 1/10/2016.
+ */
 public class ClubActivity extends AppCompatActivity {
+    private PagerSlidingTabStrip tabs;
+    private ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        ArrayList<String> titles = new ArrayList<>();
+        titles.add("新闻");
+        titles.add("活动");
+        titles.add("简介");
+        titles.add("论坛");
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        for (String s : titles) {
+            Bundle bundle = new Bundle();
+            bundle.putString("title", s);
+            fragments.add(FragmentContent.getInstance(bundle));
+        }
+        pager.setAdapter(new MyFrPagerAdapter(getSupportFragmentManager(), titles, fragments));
+        tabs.setViewPager(pager);
+        pager.setCurrentItem(1);
     }
 }
