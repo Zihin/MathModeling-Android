0698e71b996271e7e1197bd873bdb8209779c772
lfl <641863262@qq.com>
2016-01-12 14:05:00 +0800

    1-11  dev

---
+++ app/src/main/java/com/neu/coder/mathmodeling/Settings/LoginAty.java
@@ -0,0 +1,15 @@
+package com.neu.coder.mathmodeling.Settings;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.neu.coder.mathmodeling.R;
+
+public class LoginAty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_login_aty);
+    }
+}
